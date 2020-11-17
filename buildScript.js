0eca9b86b4d27afb8063a80164d5b47f7292055b
Ronnie Geraghty <ronniegerag@gmail.com>
2020-11-17 13:54:35 -0500

    redid project using web template studio tool

---
+++ buildScript.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const fse = require("fs-extra");
+const childProcess = require("child_process");
+
+//Build client app
+if (fs.existsSync("./build")) {
+  fse.removeSync("./build");
+}
+childProcess.execSync("react-scripts build", { stdio: "inherit" });
+
+//Move client
+fse.moveSync("./build", "./server/build", { overwrite: true });
