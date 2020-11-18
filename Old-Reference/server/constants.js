e7e83b776fc1a23281c863a82d1067392d968cf8
Ronnie Geraghty <ronniegerag@gmail.com>
2020-11-18 09:36:19 -0500

    New Start with seperate frontend folder for react frontend

---
+++ Old-Reference/server/constants.js
@@ -0,0 +1,10 @@
+﻿const CONSTANTS = {};
+CONSTANTS.ENDPOINT = {};
+
+CONSTANTS.PORT = process.env.PORT || "3001";
+CONSTANTS.ENDPOINT.LIST = "/list";
+
+CONSTANTS.ENDPOINT.MASTERDETAIL = "/masterdetail";
+
+
+module.exports = CONSTANTS;
---
+++ Old-Reference/server/models/Sample.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// Create Schema
+const SampleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("sample", SampleSchema);
