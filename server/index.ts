c839e1e255d51c079b56ba44cfb8e9471002fdf4
Ronnie Geraghty <ronniegerag@gmail.com>
2020-11-18 12:57:45 -0500

    created server build, start & dev npm scripts

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Dingus Mingus");
 });
 
 // start the Express server
