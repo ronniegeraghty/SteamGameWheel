28099f7d4003b0282ac3ae4c9a4cec7e10dd3e90
Ronnie Geraghty <ronniegerag@gmail.com>
2020-11-20 17:23:38 -0500

    got steam api test working

--- server/routes/steamGames/getGames.ts
+++ server/routes/steamGames/getGames.ts
@@ -1,5 +1,17 @@
+import request from "request";
 export const getGames = (userName: string): string[] => {
   let games: string[] = [];
   console.log(`USERNAME: ${userName}`);
+  let steamid = "76561198358239082";
+  let url1 =
+    "https://api.steampowered.com" +
+    `/IPlayerService/GetOwnedGames/v1/?key=${process.env.STEAM_API_KEY}&steamid=${steamid}`;
+  let url2 = `https://api.steampowered.com/IPlayerService/GetOwnedGames/' +
+  'v1/?key=${process.env.STEAM_API_KEY}&steamid=${steamid}&include_appinfo=true&include_played_free_games=true`;
+  let url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${process.env.STEAM_API_KEY}&steamid=${steamid}&format=json`;
+  request.get(url, (error, response, body) => {
+    console.log(`BODY: ${body}`);
+  });
+  console.log(`RESPONSE: ${userName}`);
   return games;
 };
