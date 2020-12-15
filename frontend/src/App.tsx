34673b6e8e0489c0e9f1237892673777ce613c07
Ronnie Geraghty <ronniegerag@gmail.com>
2020-12-15 13:50:24 -0500

    Started work on header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 
 //TODO Web Template Studio: Add routes for your new pages here.
 const App = () => {
   return (
-    <React.Fragment>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Login} />
-      </Switch>
-    </React.Fragment>
+    <div className="App">
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    </div>
   );
 };
 
