b5901351212bfbd07bb677192085ef4019c690ca
Ronnie Geraghty <ronniegerag@gmail.com>
2020-12-16 16:56:50 -0500

    started work on login form in header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Login from "../Login/Login";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="Header">
       <h1 className="brand">STEAM GAME WHEEL</h1>
+      <Login />
     </div>
   );
 };
--- frontend/src/components/Login/login.tsx
+++ frontend/src/components/Login/login.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login.css";
 
 const Login = () => {
+  const [userName, setUserName] = useState<string | undefined>(undefined);
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="Login">
+      <form className="LoginForm">
+        <input
+          className="UserNameInput"
+          type="text"
+          name="SteamUserName"
+          value={userName}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
