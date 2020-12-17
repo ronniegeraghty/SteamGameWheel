60e72aeb2f62d6f741821cb39c1d2066df5c3df9
Ronnie Geraghty <ronniegerag@gmail.com>
2020-12-17 13:26:52 -0500

    finished styling login component and added some functionality for entering the username

--- frontend/src/components/Login/login.tsx
+++ frontend/src/components/Login/login.tsx
@@ -2,17 +2,29 @@ import React, { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
-  const [userName, setUserName] = useState<string | undefined>(undefined);
+  const [userName, setUserName] = useState<string>("");
+  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setUserName(e.target.value);
+  };
+  const login = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`USERNAME: ${userName}`);
+  };
   return (
     <div className="Login">
-      <form className="LoginForm">
+      <form className="LoginForm" onSubmit={login}>
         <input
           className="UserNameInput"
           type="text"
           name="SteamUserName"
           value={userName}
+          placeholder="Steam Username"
+          onChange={handleUserNameChange}
         />
-        <button type="submit">Login</button>
+        <button className="LoginButton" type="submit">
+          Login
+        </button>
       </form>
     </div>
   );
