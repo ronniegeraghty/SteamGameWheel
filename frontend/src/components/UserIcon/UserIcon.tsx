32cfa28269ed6a061f5b5bf6b868145931ebc2c2
Ronnie Geraghty <ronniegerag@gmail.com>
2020-12-18 15:29:51 -0500

    started work on the UserIconMenu

--- frontend/src/components/UserIcon/UserIcon.tsx
+++ frontend/src/components/UserIcon/UserIcon.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import AppState from "../../interfaces/AppState";
 import "./UserIcon.css";
+import UserIconMenu from "./UserIconMenu";
 type propsType = {
   appState: AppState;
 };
 const UserIcon = ({ appState }: propsType) => {
+  const [showUserIconMenau, setShowUserIconMenau] = useState<boolean>(false);
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setShowUserIconMenau(!showUserIconMenau);
+  };
   return (
-    <div className="UserIcon">
+    <div className="UserIcon" onClick={handleClick}>
       <div className="UserName">{appState.userInfo?.personaname}</div>
-      <img className='SteamAvatar' src={appState.userInfo?.avatarfull} alt="Steam Avatar" />
+      <img
+        className="SteamAvatar"
+        src={appState.userInfo?.avatarfull}
+        alt="Steam Avatar"
+      />
+      {showUserIconMenau && <UserIconMenu />}
     </div>
   );
 };
---
+++ frontend/src/components/UserIcon/UserIconMenu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserIconMenu = () => {
+  return (
+    <div>
+      <ul>
+        <li>Logoff</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+};
+
+export default UserIconMenu;
