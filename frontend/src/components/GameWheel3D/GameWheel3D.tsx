9c2deaac7cfa56eaec505a96895ebaf9cf2212c5
Ronnie Geraghty <ronniegerag@gmail.com>
2021-02-28 17:09:01 -0500

    Got different sides of the wheel to be different colors

--- frontend/src/components/GameWheel3D/GameWheel3D.tsx
+++ frontend/src/components/GameWheel3D/GameWheel3D.tsx
@@ -1,33 +1,34 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MeshProps, useFrame } from "react-three-fiber";
-import { Mesh } from "three";
+import { Mesh, MeshPhongMaterial } from "three";
 
 const GameWheel3D: React.FC<MeshProps> = (props) => {
   const mesh = useRef<Mesh>();
-
-  const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
-
+  const [materials, setMaterials] = useState<MeshPhongMaterial[]>([]);
+  useEffect(() => {
+    var materialTop = new MeshPhongMaterial({ color: "red" });
+    var materialSide = new MeshPhongMaterial({ color: "blue" });
+    var materialBottom = new MeshPhongMaterial({ color: "yellow" });
+    var materialsArray = [];
+    materialsArray.push(materialTop);
+    materialsArray.push(materialSide);
+    materialsArray.push(materialBottom);
+    setMaterials(materialsArray);
+  }, []);
   useFrame(() => {
-    if (mesh.current) mesh.current.rotation.y += 0.01;
+    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
   });
   return (
     <mesh
-      userData={{ hello: "world" }}
       position={[0, 0, 0]}
       ref={mesh}
+      material={materials}
       scale={active ? [2, 2, 2] : [1, 1, 1]}
       rotation={[0.2, 0, 0]}
       onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => {
-        setHovered(true);
-      }}
-      onPointerOut={(event) => {
-        setHovered(false);
-      }}
     >
       <cylinderBufferGeometry args={[1, 1, 1, 6]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
   );
 };
