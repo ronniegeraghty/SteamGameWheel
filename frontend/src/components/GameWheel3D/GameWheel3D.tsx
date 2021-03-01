0d0cca6e5226defb02aae98fd21fdbfa3cbd94af
Ronnie Geraghty <ronniegerag@gmail.com>
2021-03-01 16:44:25 -0500

    Got different sides of rhe cylinder to be differnt colors using a group of multiple partial cylinders.

--- frontend/src/components/GameWheel3D/GameWheel3D.tsx
+++ frontend/src/components/GameWheel3D/GameWheel3D.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { MeshProps, useFrame } from "react-three-fiber";
-import { Mesh, MeshPhongMaterial } from "three";
+import { Group, Mesh, MeshPhongMaterial } from "three";
+import { getSourceMapRange } from "typescript";
 
 const GameWheel3D: React.FC<MeshProps> = (props) => {
   const mesh = useRef<Mesh>();
+  const group = useRef<Group>();
   const [active, setActive] = useState(false);
   const [materials, setMaterials] = useState<MeshPhongMaterial[]>([]);
   useEffect(() => {
@@ -17,19 +19,49 @@ const GameWheel3D: React.FC<MeshProps> = (props) => {
     setMaterials(materialsArray);
   }, []);
   useFrame(() => {
-    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    //if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    if (group.current)
+      group.current.rotation.x = group.current.rotation.y += 0.01;
   });
   return (
-    <mesh
-      position={[0, 0, 0]}
-      ref={mesh}
-      material={materials}
-      scale={active ? [2, 2, 2] : [1, 1, 1]}
-      rotation={[0.2, 0, 0]}
-      onClick={(event) => setActive(!active)}
-    >
-      <cylinderBufferGeometry args={[1, 1, 1, 6]} />
-    </mesh>
+    <group ref={group}>
+      <mesh
+        position={[0, 0, 0]}
+        ref={mesh}
+        scale={active ? [2, 2, 2] : [1, 1, 1]}
+        rotation={[0.2, 0, 0]}
+        onClick={(event) => setActive(!active)}
+      >
+        <cylinderBufferGeometry
+          args={[1, 1, 1, 1, 1, false, 0, (2 * Math.PI) / 3]}
+        />
+        <meshStandardMaterial color={"hotpink"} />
+      </mesh>
+      <mesh
+        position={[0, 0, 0]}
+        ref={mesh}
+        scale={active ? [2, 2, 2] : [1, 1, 1]}
+        rotation={[0.2, (2 * Math.PI) / 3, 0]}
+        onClick={(event) => setActive(!active)}
+      >
+        <cylinderBufferGeometry
+          args={[1, 1, 1, 1, 1, false, 0, (2 * Math.PI) / 3]}
+        />
+        <meshStandardMaterial color={"forestgreen"} />
+      </mesh>
+      <mesh
+        position={[0, 0, 0]}
+        ref={mesh}
+        scale={active ? [2, 2, 2] : [1, 1, 1]}
+        rotation={[0.2, (4 * Math.PI) / 3, 0]}
+        onClick={(event) => setActive(!active)}
+      >
+        <cylinderBufferGeometry
+          args={[1, 1, 1, 1, 1, false, 0, (2 * Math.PI) / 3]}
+        />
+        <meshStandardMaterial color={"slateblue"} />
+      </mesh>
+    </group>
   );
 };
 
