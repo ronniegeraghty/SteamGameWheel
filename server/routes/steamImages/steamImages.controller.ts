030d0702a5430120cbacd0dea42cd50d205d873f
Ronnie Geraghty <ronniegerag@gmail.com>
2021-04-29 08:27:03 -0400

    Created API to pull Steam game images, Fixes #32

--- server/routes/steamImages/steamImages.controller.ts
+++ server/routes/steamImages/steamImages.controller.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import fetch from "node-fetch";
+import request from "request";
 import Controller from "../../interfaces/controller.interface";
 
 class SteamImages implements Controller {
@@ -11,43 +12,27 @@ class SteamImages implements Controller {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}/`, this.getImage);
+    this.router.get(`${this.path}/:appid/:img_logo_url`, this.getImage);
   }
 
-  private getImage = async (request: Request, response: Response) => {
+  private getImage = async (req: Request, res: Response) => {
     //Steam image upl example
     //`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_logo_url}.jpg`
     if (
-      request.body.appid &&
-      request.body.img_logo_url &&
-      typeof request.body.appid === "string" &&
-      typeof request.body.img_logo_url === "string"
+      req.params.appid &&
+      req.params.img_logo_url &&
+      typeof req.params.appid === "string" &&
+      typeof req.params.img_logo_url === "string"
     ) {
-      const { appid, img_logo_url } = request.body;
+      const { appid, img_logo_url } = req.params;
       const host: string = `http://media.steampowered.com`;
       const path: string = `/steamcommunity/public/images/apps/`;
       const url: string = host + path + appid + "/" + img_logo_url + ".jpg";
-      await fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(res.statusText);
-          }
-          return res.blob;
-        })
-        .then((blob) => {
-          response.send(blob);
-        });
+      request.get(url).pipe(res);
     } else {
-      response.send(
-        "Request must include an appid:string and img_logo_url:string"
-      );
+      res.send("Request must include an appid:string and img_logo_url:string");
     }
-    // console.log(`Request Body: ${JSON.stringify(request.body)}`);
-    // request.body;
-    // response.send("We Online!");
   };
-
-  private fetchImage = (appid: string, img_logo_url: string) => {};
 }
 
 export default SteamImages;
