1d6bef7c9aaa5d4a752599ddaa96f55cf581ef0f
Ronnie Geraghty <ronniegerag@gmail.com>
2021-04-28 22:14:52 -0400

    working on fetching images

--- server/routes/steamImages/steamImages.controller.ts
+++ server/routes/steamImages/steamImages.controller.ts
@@ -15,10 +15,39 @@ class SteamImages implements Controller {
   }
 
   private getImage = async (request: Request, response: Response) => {
+    //Steam image upl example
     //`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_logo_url}.jpg`
-    console.log(`Request Body: ${JSON.stringify(request.body)}`);
-    request.body;
-    response.send("We Online!");
+    if (
+      request.body.appid &&
+      request.body.img_logo_url &&
+      typeof request.body.appid === "string" &&
+      typeof request.body.img_logo_url === "string"
+    ) {
+      const { appid, img_logo_url } = request.body;
+      const host: string = `http://media.steampowered.com`;
+      const path: string = `/steamcommunity/public/images/apps/`;
+      const url: string = host + path + appid + "/" + img_logo_url + ".jpg";
+      await fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(res.statusText);
+          }
+          return res.blob;
+        })
+        .then((blob) => {
+          response.send(blob);
+        });
+    } else {
+      response.send(
+        "Request must include an appid:string and img_logo_url:string"
+      );
+    }
+    // console.log(`Request Body: ${JSON.stringify(request.body)}`);
+    // request.body;
+    // response.send("We Online!");
   };
+
+  private fetchImage = (appid: string, img_logo_url: string) => {};
 }
+
 export default SteamImages;
